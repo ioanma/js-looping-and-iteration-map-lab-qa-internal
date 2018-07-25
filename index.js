a60727b6422919cc4f910f177ecbaa957e49163a
Marios Ioannidis <ioanmar80@gmail.com>
2018-07-25 17:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  let newDrivers = drivers.slice();
+  newDrivers = newDrivers.map(toLowerCase());
+  return newDrivers;
+}
+
+function nameToAttributes(drivers) {
+  const driverNames = drivers.map(str.split(" "));
+  const driverObject = {
+    firstName: driverNames[0],
+    lastName: driverNames[1],
+  };
+  return driverObject;
+}
+
+function attributesToPhrase(drivers) {
+  
+}
